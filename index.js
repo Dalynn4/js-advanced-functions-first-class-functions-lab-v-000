0f59592d404317cf1d221189c54984549e0c48e4
Randall Robinson <keith_adams007@yahoo.com>
2018-05-20 18:32:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 // Code your solution in this file!
-const first = function (array) {  
-  const newarr = []
+
+  function returnFirstTwoDrivers (array) {
+   const newarr = []
    newarr.push(array[0])
    newarr.push(array[1])
    return newarr
 }
-
-
-  function returnFirstTwoDrivers (array) {
-   first(array)
-  }
 
