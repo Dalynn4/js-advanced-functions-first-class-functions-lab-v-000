a05a77a3bd4f64e7079d3975d4c968bf7259ea1a
Randall Robinson <keith_adams007@yahoo.com>
2018-05-20 18:30:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,5 @@ const first = function (array) {
 
 
   function returnFirstTwoDrivers (array) {
-   const newarr = []
-   newarr.push(array[0])
-   newarr.push(array[1])
-   return newarr
-}
+   first
 
