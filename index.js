49864cf0d1c10eb9de7348c16a8a5ff6ee3c158d
Randall Robinson <keith_adams007@yahoo.com>
2018-05-20 18:32:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 
-  function returnFirstTwoDrivers (array) {
+  const buttsex = function returnFirstTwoDrivers (array) {
    const newarr = []
    newarr.push(array[0])
    newarr.push(array[1])
